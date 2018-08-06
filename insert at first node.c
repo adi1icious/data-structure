f0f51a344809c39b4ec1588768e1912e9847d7df
Aditya Sharma <37013856+adi1icious@users.noreply.github.com>
2018-08-06 22:31:35 +0530

    Add files via upload

---
+++ insert at first node.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node
+{
+	int  data;
+	struct Node* next; };
+
+void InsertAtFirst(struct Node* S, int data)
+{
+	struct Node* N= (struct Node*)(malloc(sizeof(struct Node))) ;
+	if(N==NULL)
+	printf("Failed to Allocate Memory");
+	else
+	{
+		N->data=data;
+		N->next=NULL;
+        if(S==NULL) S=N;
+		else{
+			N->next=S;
+			S=N; }
+	}
+}
